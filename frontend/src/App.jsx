7ac91e003fe380aa70ffb9a63f47d0bc8c8bd0d3
aneeshraj05 <aneesh.rajx@gmail.com>
2026-03-28 20:40:39 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div>Hello, World!
+        <h1>Welcome to My App</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
